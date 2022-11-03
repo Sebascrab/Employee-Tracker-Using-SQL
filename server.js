8b1f52b8971e9f372f22f09d2cdeb9b7bc2130d5
Alexander <alexanders.tischner@outlook.com>
2022-11-02 22:40:14 -0600

    added questions to server.js file

--- server.js
+++ server.js
@@ -21,16 +21,137 @@ app.use(express.json());
 // connecting to mysql database:
 const db = mysql.createConnection(
     {
-        host: 'localhost', 
+        host: 'localhost',
         user: 'root',
-        password: '3577', 
+        password: '3577',
         database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
 );
 
+// prompts user to answer inital questions
+const questions = () => {
+    inquier.prompt([
+        {
+            type: 'list',
+            name: 'view',
+            message: 'Welcome to your team! Please make a selection from the following options:',
+            choices: ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add Department',
+                'Add role',
+                'Add Employee',
+                'Update an employee role',
+                'Done']
+        }
+    ])
+
+    // switch that calls functions depending on selection 
+        .then((answers) => {
+            switch (answers.view) {
+                case 'View all departments':
+                    viewDepartments();
+                    break
+                case 'View all roles':
+                    viewRoles();
+                    break
+                case 'View all employees':
+                    viewEmployees();
+                    break
+                case 'Add Department':
+                    addDepartment();
+                    break
+                case 'Add role':
+                    addRole();
+                    break
+                case 'Add Employee':
+                    addEmployee();
+                    break
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break
+                case 'Done':
+                    db.end()
+                    break
+            }
+        })
+};
 
 
+
+
+
+// functions:
+
+
+
+
+// function to show departments:
+viewDepartments = () => {
+    console.log('All Departments. \n');
+
+    
+}
+
+// function to view all roles:
+viewRoles = () => {
+    console.log('All Roles. \n');
+
+
+}
+
+// function to view all employees:
+viewEmployees = () => {
+    console.log('All Employees. \n');
+
+
+}
+
+// function to add department:
+addDepartment = () => {
+    inquier.prompt([
+        {
+            type: 'input',
+            name: 'addDepartment',
+            message: 'What is the name of the department you would like to add?',
+        }
+    ])
+    .then(answer => {
+
+
+        console.log('You have added ' + answer.addDepartment + ' to the list of Departments');
+    })
+};
+
+// function to add role: 
+addRole = () => {
+    inquier.prompt([
+        {
+            type: 'input', 
+            name: 'addRole', 
+            message: 'What is the name of the role you would like to add?',
+        },
+        {
+            type: 'input', 
+            name: 'addSalary', 
+            message: 'How much is the salary for this role?'
+        },
+        {
+            type: 'input', 
+            name: 'addDep', 
+            message: 'What is the name of the deparment this role will be in?',
+        }
+    ])
+    .then(answer => {
+
+
+        console.log('You have added ' + answer.addRole + ' to the list of Roles');
+    })
+};
+
+// function to add Employee
+ 
 
 
 
